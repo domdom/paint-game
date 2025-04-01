5e7a8686dd29a144c4235faf88732dbebeb14742
dom <domdom@users.noreply.github.com>
2025-04-01 13:13:15 +1000

    WIP: try adding service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_VERSION = "paint-game-v1";
+const resources = [
+    "/",
+    "/index.html",
+    "/js/board.js",
+    "/js/cell.js",
+    "/js/levels.js",
+    "/js/ui.js",
+
+    "/favicon.ico",
+    "/icon.svg",
+    "/icon.png",
+    "/site.webmanifest",
+];
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(CACHE_VERSION);
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache(resources),
+    );
+});
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open(CACHE_VERSION);
+  await cache.put(request, response);
+};
+
+const fetchFirst = async (event, request) => {
+    try {
+        const netResponse = await fetch(request);
+        if (netResponse.ok) {
+            event.waitUntil(putInCache(request, netResponse.clone()));
+        } else {
+            const cacheResponse = await caches.match(request);
+            if (cacheResponse) {
+                return cacheResponse;
+            }
+        }
+        return netResponse;
+    } catch (error) {
+        const cacheResponse = await caches.match(request);
+        if (cacheResponse) {
+            return cacheResponse;
+        }
+        return new Response("Failed: " + error.message, {
+            status: 408,
+            headers: { "Content-Type": "text/plain" },
+        });
+    }
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetchFirst({ event: event, request: event.request }),
+  );
+});
