25f7ea7d778f6681384ba092d0ea972443604bec
dom <domdom@users.noreply.github.com>
2025-06-07 18:39:30 +1000

    levels: add the longest snake level, heart level always targets red

--- js/levels.js
+++ js/levels.js
@@ -3,6 +3,15 @@ const CellKind = {
     Random: 1,
     Empty: null,
 };
+const CellColors = {
+    Random: 0,
+    Red: 1,
+    Orange: 2,
+    Yellow: 3,
+    Green: 4,
+    Blue: 5,
+    Purple: 6,
+};
 let LEVELS = (() => {
     let E = CellKind.Empty;
     let R = CellKind.Random;
@@ -88,7 +97,21 @@ let LEVELS = (() => {
             ]
         },
         {
-            name: 'dom loves you!', maxMoves: 25, grid: [
+            name: 'snnnnnnnaaaaaaaaaakkkkkkkkeeeee', maxMoves: 100, grid: [
+                [S, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R,],
+                [R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R,],
+                [R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R,],
+                [R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R,],
+                [R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R,],
+                [R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R,],
+                [R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R,],
+                [R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R, E, E, R, R,],
+                [R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R, R, R, R, R, E, E, R, R,],
+                [R, R, R, R, R, R, E, E, R, S, R, R, R, R, E, E, R, S, R, R, R, R, E, E, R, S, R, R, R, R, E, E, R, S, R, R, R, R, E, E, R, S, R, R, R, R, E, E, R, S, R, R, R, R, E, E, R, S,],
+            ]
+        },
+        {
+            name: 'dom loves you!', target: CellColors.Red, maxMoves: 25, grid: [
                 [E, E, E, R, E, E, E, R, E, E, E,],
                 [E, R, R, R, R, E, R, R, R, R, E,],
                 [R, R, R, R, R, R, R, R, R, R, R,],
